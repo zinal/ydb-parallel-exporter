1aafd6fe4ead655760ba368f28931c01e4067ce8
Maksim Zinal <zinal@ydb.tech>
2025-05-20 17:07:01 +0300

    initial commit

---
+++ src/main/java/tech/ydb/samples/exporter/App.java
@@ -0,0 +1,12 @@
+package tech.ydb.samples.exporter;
+
+/**
+ *
+ * @author zinal
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
